0f6edae35ba271b531e6efd48d79d230dd949df0
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2024-08-12 00:09:10 +0530

    On branch feature/figma-design  - add scss and bootstrap

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  console.log('hello');
+  return (
+    <div>
+      <p>ss</p>
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default Home;
